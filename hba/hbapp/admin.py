281c9f6cf50afe4274322910e723f34f7505e2c6
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-11-07 19:27:12 +0530

    first commit

--- hba/hbapp/admin.py
+++ hba/hbapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Register your models here.
+@receiver(post_save,sender=Hospital)
+def afterhospitalsave(signal, instance,**kwargs):
+    service=Service(hospital=instance)
+    service.save()
+    
+
+    
+
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(Hospital)
+admin.site.register(Service)
+
+
--- hba/hbapp/models.py
+++ hba/hbapp/models.py
@@ -3,14 +3,20 @@ from django.db import models
 # Create your models here.
 class State(models.Model):
     name=models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return self.name
 class City(models.Model):
     name=models.CharField(max_length=50)
     state=models.ForeignKey(State,on_delete=models.CASCADE,related_name='cities')
+    def __str__(self) -> str:
+        return self.name
 class Hospital(models.Model):
     name=models.CharField(max_length=50)
     city=models.ForeignKey(City,on_delete=models.CASCADE,related_name='hospitals')
     phone=models.CharField(max_length=12)
     address=models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.name
 class Service(models.Model):
     hospital=models.OneToOneField(Hospital,on_delete=models.CASCADE,primary_key=True)
     oxygen_beds_total=models.IntegerField(default=0)
@@ -18,6 +24,8 @@ class Service(models.Model):
     oxygen_cylinder_total=models.IntegerField(default=0)
     oxygen_cylinder_available=models.IntegerField(default=0)
     ventilator_available=models.IntegerField(default=0)
+    def __str__(self) -> str:
+        return self.hospital.name
     
 
 
