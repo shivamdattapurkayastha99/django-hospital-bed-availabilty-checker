d981f47d7d67fd57af6574dee3f05612192895b0
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-11-08 20:53:27 +0530

    second commit

--- hba/hbapp/admin.py
+++ hba/hbapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models.base import Model
 from .models import *
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -8,13 +9,19 @@ from django.dispatch import receiver
 def afterhospitalsave(signal, instance,**kwargs):
     service=Service(hospital=instance)
     service.save()
-    
+class ServiceAdmin(admin.ModelAdmin):
+    model=Service
+    list_display=['hospital','oxygen_beds_total','oxygen_beds_available','oxygen_cylinder_total','oxygen_cylinder_available','ventilator_available']
+
+class HospitalAdmin(admin.ModelAdmin):
+    model=Hospital
+    list_display=['name','phone','address','city']
 
     
 
 admin.site.register(State)
 admin.site.register(City)
-admin.site.register(Hospital)
-admin.site.register(Service)
+admin.site.register(Hospital,HospitalAdmin)
+admin.site.register(Service,ServiceAdmin)
 
 
