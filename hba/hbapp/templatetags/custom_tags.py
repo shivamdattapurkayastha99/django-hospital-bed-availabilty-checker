fdfbf8101c2d2d674aa6d09732379407f671e95f
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-12-01 22:48:05 +0530

    second commit

--- hba/hbapp/templatetags/custom_tags.py
+++ hba/hbapp/templatetags/custom_tags.py
@@ -1,4 +1,6 @@
 from django import template
+
+from hbapp.models import Availability
 register=template.Library()
 @register.simple_tag
 def get_table_class(value):
@@ -6,3 +8,7 @@ def get_table_class(value):
         return 'bg-success text-light'
     else:
         return 'bg-danger text-light'
+@register.simple_tag
+def get_availabilities(hospital):
+    return Availability.objects.filter(hospital=hospital).order_by('facility_id')
+    
--- hba/hbapp/views.py
+++ hba/hbapp/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render,HttpResponse
-from .models import Facility,State,City
+from .models import Availability, Facility, Hospital,State,City
 
 # Create your views here.
 def home(request):
     facilities=Facility.objects.all().order_by('title')
     cities=City.objects.all()
     states=State.objects.all()
+    hospitals=Hospital.objects.all()
+    availabilities=Availability.objects.all()
 
     context={
         'facilities':facilities,
         'cities':cities,
-        'states':states
+        'states':states,
+        'hospitals':hospitals,
+        'availabilities':availabilities
     }
     return render(request,'hbapp/index.html',context=context)
     
